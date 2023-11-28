d867ae4086ccca63d3704643d82a9a9a4830f66a
Ronald Saenz <rjsaenz@espol.edu.ec>
2023-11-28 16:29:59 -0500

    Changes for deployment

--- backend/server.js
+++ backend/server.js
@@ -4,9 +4,13 @@ const cors = require('cors');
 require('dotenv').config();
 
 const app = express();
+const path = require("path");
 
 app.use(cors());
 app.use(express.json()); // Body parser
+
+// Serve static files from the React app
+app.use(express.static(path.join(__dirname, "build")));
 
 // Connect to MongoDB
 mongoose.connect(process.env.MONGODB_URI, {
@@ -17,6 +21,9 @@ mongoose.connect(process.env.MONGODB_URI, {
   .catch(err => console.error('Could not connect to MongoDB', err));
 
 // Define routes here...
+
+// This route serves the React app
+app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, "build", "index.html")));
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
